7da31849484f7c9cc916c6e959d1eb3487644d23
Nikolay <103628537+Nike90210@users.noreply.github.com>
2024-12-15 16:49:20 +0300

    arraised comments

--- SimbirSoftApp/View/DutyView.swift
+++ SimbirSoftApp/View/DutyView.swift
@@ -22,30 +22,30 @@ class DutyView: UIView {
     }
 
     func setView() {
-        // Настройка метки месяца
+
         monthLbl.font = .boldSystemFont(ofSize: 24)
         monthLbl.text = "Декабрь"
 
-        // Проверка версии iOS для отображения иконки
+
         if #available(iOS 13.0, *) {
             plusButton.setImage(UIImage(systemName: "plus"), for: .normal)
         } else {
-            // Для iOS 12 и ниже используем другую иконку или не используем её
+
             plusButton.setTitle("+", for: .normal)
         }
 
         plusButton.backgroundColor = .white
         plusButton.tintColor = .red
 
-        // Настройка UIDatePicker для iOS 14 и выше
+
         if #available(iOS 14.0, *) {
             dateTaskPicker.preferredDatePickerStyle = .inline
         } else {
-            // Для более старых версий используем обычный стиль
+
             dateTaskPicker.datePickerMode = .dateAndTime
         }
 
-        // Регистрация ячейки для таблицы
+
         taskTable.register(TaskCell.self, forCellReuseIdentifier: TaskCell.resuseID)
     }
 
@@ -55,28 +55,27 @@ class DutyView: UIView {
         addSubview(dateTaskPicker)
         addSubview(taskTable)
 
-        // Отключаем трансляцию масок для всех элементов
+
         monthLbl.translatesAutoresizingMaskIntoConstraints = false
         plusButton.translatesAutoresizingMaskIntoConstraints = false
         dateTaskPicker.translatesAutoresizingMaskIntoConstraints = false
         taskTable.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
-            // Ограничения для monthLbl
+
             monthLbl.topAnchor.constraint(equalTo: topAnchor, constant: 120),
             monthLbl.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 50),
 
-            // Ограничения для plusButton
+
             plusButton.topAnchor.constraint(equalTo: topAnchor, constant: 110),
             plusButton.leadingAnchor.constraint(equalTo: monthLbl.trailingAnchor, constant: 150),
             plusButton.widthAnchor.constraint(equalToConstant: 50),
             plusButton.heightAnchor.constraint(equalToConstant: 50),
 
-            // Ограничения для dateTaskPicker
             dateTaskPicker.topAnchor.constraint(equalTo: plusButton.bottomAnchor, constant: 70),
             dateTaskPicker.centerXAnchor.constraint(equalTo: centerXAnchor),
 
-            // Ограничения для taskTable
+
             taskTable.topAnchor.constraint(equalTo: dateTaskPicker.bottomAnchor, constant: 20),
             taskTable.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15),
             taskTable.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -15),
